7a46ea939fe2e3b9af97b5d65180cf3df753d99e
Nguyen Trong Hue <hue.nguyen.trong@ntq-solution.com.vn>
2021-03-04 12:45:26 +0700

    [Add] init_project

---
+++ shop/src/main/java/com/shop/shop/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.shop.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+    @GetMapping(value = {"/"})
+    String index(final ModelMap model, final HttpServletRequest request, HttpSession session, final HttpServletResponse response) {
+        return "index";
+    }
+}
+
